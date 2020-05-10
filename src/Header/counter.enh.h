fedc681284939b991202604301a0ea98c069e203
Harith Manoj <harithmanoj@gmail.com>
2020-05-10 20:13:52 +0530

    removed unnecasary <p> from file documentation

--- src/Header/counter.enh.h
+++ src/Header/counter.enh.h
@@ -9,22 +9,19 @@
 
 	Copyright 2020 Harith Manoj <harithpub@gmail.com>
 
-		<p>
 	Enhance is free software : you can redistribute it and /or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
-	</p>
-	<p>
+	
 	Enhance is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
 	GNU General Public License for more details.
-	</p>
-	<p>
+	
 	You should have received a copy of the GNU General Public License
 	along with Enhance. If not, see < https://www.gnu.org/licenses/>.
-	</p>
+
 ******************************************************************************/
 
 #ifndef COUNTER_ENH_H
