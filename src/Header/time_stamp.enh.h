c04243772980af9cd4eab003744ca13961481efa
Harith Manoj <harithpub@gmail.com>
2020-05-13 00:18:19 +0530

    Added file time_stamp.enh.h for time stamp class.

---
+++ src/Header/time_stamp.enh.h
@@ -0,0 +1,39 @@
+/** ***************************************************************************
+	\file time_stamp.enh.h
+
+	\brief File for time class.
+
+	Created 13 May 2020
+
+	This file is part of Enhance.
+
+	Copyright 2020 Harith Manoj <harithpub@gmail.com>
+
+	Enhance is free software : you can redistribute it and /or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Enhance is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Enhance. If not, see < https://www.gnu.org/licenses/>.
+
+
+******************************************************************************/
+
+#ifndef TIME_STAMP_ENH_H
+
+#define TIME_STAMP_ENH_H				time_stamp.enh.h
+
+#include "date.enh.h"
+
+namespace enh
+{
+
+}
+
+#endif
