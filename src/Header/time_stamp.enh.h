bd42fc3f98573a4aeae895c3f931ca426ef2d582
Harith Manoj <harithpub@gmail.com>
2020-05-13 21:12:35 +0530

    Added and full documented time_stamp class and added usage example

--- src/Header/time_stamp.enh.h
+++ src/Header/time_stamp.enh.h
@@ -33,7 +33,235 @@
 
 namespace enh
 {
+	class time_stamp
+	{
+		/**
+			\brief The seconds part of time [0,60].
+		*/
+		unsigned short seconds;
+		
+		/**
+			\brief The minutes part of time [0,59].
+		*/
+		unsigned short minutes;
 
+		/**
+			\brief The hours part of time [0,23].
+		*/
+		unsigned short hours;
+
+	public:
+
+		/**
+			\brief Sets the time to the time indicated by arguments.
+
+			<h3>Exception</h3>
+			Throws <code>std::invalid_argument</code> if sec, min, hr is
+			not within bounds. [0,60], [0,59], [0,23] respectively.
+		*/
+		inline void set(
+			unsigned short sec /**< : <i>in</i> : The seconds field [0,60].*/,
+			unsigned short min /**< : <i>in</i> : The minutes field [0,59].*/,
+			unsigned short hr  /**< : <i>in</i> : The hours field [0,59].*/
+		)
+		{
+			if (!isConfined<unsigned short>(sec, 0, 60, true, true))
+				throw std::invalid_argument("Seconds should be in range"
+					" [0,60]");
+			if (!isConfined<unsigned short>(min, 0, 59, true, true))
+				throw std::invalid_argument("Minutes should be in range"
+					" [0,59]");
+			if (!isConfined<unsigned short>(hr, 0, 23, true, true))
+				throw std::invalid_argument("Hours should be in range"
+					" [0,23]");
+
+			seconds = sec;
+			minutes = min;
+			hours = hr;
+		}
+
+		/**
+			\brief Sets the time to the time indicated by argument.
+		*/
+		inline void set(
+			time_t timeStamp /**< : <i>in</i> : The time to set.*/
+		)
+		{
+			tm tm_str;
+			enh::localtime(&tm_str, &timeStamp);
+			set(tm_str.tm_sec, tm_str.tm_min, tm_str.tm_hour);
+		}
+
+		/**
+			\brief Sets the time to the current time.
+		*/
+		inline void set()
+		{
+			set(std::time(nullptr));
+		}
+
+		/**
+			\brief Sets the time to the time indicated by arguments.
+
+			<h3>Exception</h3>
+			Throws <code>std::invalid_argument</code> if sec, min, hr is
+			not within bounds. [0,60], [0,59], [0,23] respectively.
+		*/
+		inline time_stamp(
+			unsigned short sec /**< : <i>in</i> : The seconds field [0,60].*/,
+			unsigned short min /**< : <i>in</i> : The minutes field [0,59].*/,
+			unsigned short hr  /**< : <i>in</i> : The hours field [0,59].*/
+		)
+		{
+			set(sec, min, hr);
+		}
+
+		/**
+			\brief Sets the time to the time indicated by argument.
+		*/
+		inline time_stamp(
+			time_t timeStamp /**< : <i>in</i> : The time to set.*/
+		)
+		{
+			set(timeStamp);
+		}
+
+		/**
+			\brief Sets the time to the current time.
+		*/
+		inline time_stamp()
+		{
+			set();
+		}
+
+		/**
+			\brief Adds One Hour to the time stamp,
+			if Hour is 23, resets to 0.\n
+
+			<h3>Return</h3>
+			Returns true if Hour is reset to 0.
+		*/
+		inline bool addHour()
+		{
+			if (hours >= 23)
+			{
+				hours = 0;
+				return true;
+			}
+			else
+				++hours;
+		}
+
+		/**
+			\brief Adds One Minutes to the time stamp,
+			if Minutes is 59, resets to 0, Adds One Hour.\n
+
+			<h3>Return</h3>
+			Returns true if Hour is reset to 0.
+		*/
+		inline bool addMinutes()
+		{
+			if (minutes >= 59)
+			{
+				minutes = 0;
+				return addHour();
+			}
+			else
+				++minutes;
+		}
+
+		/**
+			\brief Adds One Seconds to the time stamp,
+			if Seconds is 59, resets to 0, Adds One Minutes.\n
+
+			<h3>Return</h3>
+			Returns true if Hour is reset to 0.
+		*/
+		inline bool addSeconds()
+		{
+			if (seconds >= 59)
+			{
+				seconds = 0;
+				return addMinutes();
+			}
+			else
+				++seconds;
+		}
+
+		/**
+			\brief Get Seconds field.
+		*/
+		inline unsigned short getSeconds() const noexcept { return seconds; }
+
+		/**
+			\brief Get Minutes field.
+		*/
+		inline unsigned short getMinutes() const noexcept { return minutes; }
+
+		/**
+			\brief Get Hours field.
+		*/
+		inline unsigned short getHours() const noexcept { return hours; }
+
+		/**
+			\brief Get The time as a string in custom format.
+
+			Pass Argument as a string containing any of the following
+			components.
+
+			<b>sec</b> : The seconds.\n
+			<b>min</b> : The minutes.\n
+			<b>hour</b> : The hours.\n
+
+			Example : <code>"secs minm hourhr"</code> : <code>"25s 25m 10hr"
+				</code>\n
+
+			<h3>Overload</h3>
+			-# <code>inline std::string getStringTime() const;<code>\n
+		*/
+		inline std::string getStringTime() const
+		{
+			return signExtendValue(hours, 2) + " : "
+				+ signExtendValue(minutes, 2) + " : "
+				+ signExtendValue(seconds, 2);
+		}
+
+		/**
+			\brief Get The time as a string in custom format.
+
+			Pass Argument as a string containing any of the following
+			components.
+
+			<b>sec</b> : The seconds.\n
+			<b>min</b> : The minutes.\n
+			<b>hour</b> : The hours.\n
+
+			Example : <code>"secs minm hourhr"</code> : <code>"25s 25m 10hr"
+				</code>\n
+
+			<h3>Overload</h3>
+			-# <code>inline std::string getStringTime() const;<code>\n
+		*/
+		inline std::string getStringTime(
+			std::string format /**< : <i>in</i> : The format of date.*/
+		) const
+		{
+			std::size_t psec, pmin, phour;
+			psec = format.find("sec");
+			if (psec != std::string::npos)
+				format.replace(psec, 3, signExtendValue(seconds, 2));
+			
+			pmin = format.find("min");
+			if (pmin != std::string::npos)
+				format.replace(pmin, 3, signExtendValue(minutes, 2));
+			
+			phour = format.find("hour");
+			if (phour != std::string::npos)
+				format.replace(phour, 4, signExtendValue(hours, 2));
+
+			return format;
+		}
+	};
 }
 
 #endif
