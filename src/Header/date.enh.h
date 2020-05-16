635ce0c76844c963a38205931322821f466bda6d
Harith Manoj <harithpub@gmail.com>
2020-05-16 22:49:59 +0530

    Added multiple getters

--- src/Header/date.enh.h
+++ src/Header/date.enh.h
@@ -433,8 +433,43 @@ namespace enh
 			return yrday.get();
 		}
 
+		/**
+			\brief The name of the day.
+		*/
+		inline std::string getDayOfWeekString() const
+		{
+			switch (wkday.get())
+			{
+			case 0: return "Sunday";
+			case 1: return "Monday";
+			case 2: return "Tuesday";
+			case 3: return "Wednesday";
+			case 4: return "Thursday";
+			case 5: return "Friday";
+			case 6: return "Saturday";
+			default: return "Error";
+			}
+		}
 
+		/**
+			\brief The name of the day (shortened).
+		*/
+		constexpr inline std::string_view getShortDayOfWeekString() const
+		{
+			switch (wkday.get())
+			{
+			case 0: return "Sun";
+			case 1: return "Mon";
+			case 2: return "Tue";
+			case 3: return "Wed";
+			case 4: return "Thu";
+			case 5: return "Fri";
+			case 6: return "Sat";
+			default: return "Error";
+			}
+		}
 
+		
 	};
 	
 
