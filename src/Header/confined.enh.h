8836ffd07245a5f85f83baaf893cb8c37577ff73
Harith Manoj <harithmanoj@gmail.com>
2020-05-16 12:39:30 +0530

    added class confined_base, al its data members, type alias to be used, constructor, set, get and add functions

--- src/Header/confined.enh.h
+++ src/Header/confined.enh.h
@@ -28,12 +28,85 @@
 
 #define CONFINED_ENH_H
 
-#include "general.enh.h"
+#include <functional>
+#include <stdexcept>
+#include "framework.enh.h"
 
 
 namespace enh
 {
+	template<class integral>
+	class confined_base
+	{
+	public:
+		using value_type = integral;
+		using pred_t = std::function<bool(value_type)>;
+		using limit_t = std::function<value_type()>;
+	private:
+		value_type value;
+		pred_t uLimit_pred;
+		pred_t lLimit_pred;
+		limit_t uLimit;
+		limit_t lLimit;
+	public:
+		
+		inline constexpr confined_base(
+			const pred_t &upper_p,
+			const pred_t &lower_p,
+			const limit_t& upper_l,
+			const limit_t& lower_l,
+			const value_type &val
+		) : uLimit_pred(upper_p), lLimit_pred(lower_p), value(val),
+			uLimit(upper_l), lLimit(lower_l)
+		{
+			if (uLimit() < lLimit())
+				throw std::invalid_argument("upper limit should be greater than lower");
+			if (!uLimit_pred(val) || !lLimit_pred(val))
+			{
+				value = lLimit() + (uLimit() - lLimit()) / 2;
+				throw std::invalid_argument("value not within limits");
+			}
+		}
 
+		inline constexpr set(
+			const value_type &val
+		)
+		{
+			if (!uLimit_pred(val) || !lLimit_pred(val))
+			{
+				throw std::invalid_argument("value not within limits");
+			}
+			value = val;
+		}
+
+		inline constexpr value_type get() const noexcept { return value; }
+
+		inline constexpr unsigned add() noexcept
+		{
+			++value;
+			if (!uLimit_pred(value))
+			{
+				value = lLimit();
+				return 1;
+			}
+			return 0;
+		}
+
+		inline constexpr unsigned add(value_type additional) noexcept
+		{
+			value_type rem = additional % (uLimit() - lLimit());
+			value_type ret = additional / (uLimit() - lLimit());
+
+			value += rem;
+			if (!uLimit_pred(value))
+			{
+				value = value + lLimit() - uLimit();
+				++ret;
+			}
+
+			return ret;
+		}
+	};
 }
 
 
