966bec59f91db60d1f865a27f234c0aa48334c29
Harith Manoj <harithpub@gmail.com>
2020-06-05 20:05:02 +0530

    Initial test case for QProc

--- auto-test/QProc.test.cpp
+++ auto-test/QProc.test.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <queued_process.enh.h>
 
-
-
 void main()
 {
-	std::cout << "epb";
+	unsigned t = 0;
+	enh::queued_process<unsigned> tQ;
+	tQ.RegisterProc(
+		[&](unsigned a) -> enh::tristate {t += a; return enh::tristate::GOOD; }
+	);
+
+	unsigned exp = 0;
+
+	for (unsigned i = 0; i < 5; ++i)
+	{
+		exp += i;
+		tQ.postMessage(i);
+	}
+
+	tQ.safe_join(std::chrono::milliseconds(1));
+
+	if (t != exp)
+	{
+		std::cerr << "QProc.test.cpp : Test 0 : Not evaluating all messages.";
+		return 1;
+	}
+	else
+	{
+		std::cerr << "QProc.test.cpp : Test 0 : Has evaluated all messages.";
+	}
 }
