56f61461ee1d78e1f1317a3a0965cb151f99d38f
Harith Manoj <harithpub@gmail.com>
2020-06-06 12:40:17 +0530

    Added ASSERT_CONTINUE macro to make intermediate but not final checks

--- auto-test/test.base.h
+++ auto-test/test.base.h
@@ -59,13 +59,19 @@ inline int call_main()
 	return ret_val;
 }
 
-#define CONCAT(x,y)								x##y
+#define CONCAT(x,y)									x##y
 
-#define REGISTER_TEST(function)					testBase::allTestCases.push_back(function)
+#define REGISTER_TEST(function)						testBase::allTestCases.push_back(function)
 
-#define REGISTER_MEM_TEST(function)				testBase::allTestCases.push_back([this](){return function();})
+#define REGISTER_MEM_TEST(function)					testBase::allTestCases.push_back([this](){return function();})
 
-#define ASSERT_TEST(condition, fail_message)	return testBase::assertTest(condition, #condition, fail_message, __func__, __FILE__ )
+#define ASSERT_TEST(condition, fail_message)		return testBase::assertTest(condition, #condition, fail_message, __func__, __FILE__ )
+
+#define ASSERT_CONTINUE(condition, fail_message)	if(!condition)\
+													{\
+														std::cerr << "FAIL : " << __FILE__ << " : " << __func__ << " : condition " << #condition << " : " << fail_message << "\n";\
+														return false;\
+													}
 												
 
 
