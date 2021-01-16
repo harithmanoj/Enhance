c1c7b8453d9eb3438e8aa5af93601eb5d9aae737
Harith Manoj <harithpub@gmail.com>
2021-01-16 22:44:06 +0530

    moved example compilation to single file main.ex.cpp depending on #defines

---
+++ documentation/examples/main.ex.cpp
@@ -0,0 +1,48 @@
+
+#ifdef ConfinedValueExample
+#	include "ConfinedValue.ex.cpp"
+#endif
+
+#ifdef CounterExample
+#	include "Counter.ex.cpp"
+#endif
+
+#ifdef DateExample
+#	include "Date.ex.cpp"
+#endif
+
+#ifdef DateTimeExample
+#	include "DateTime.ex.cpp"
+#endif
+
+#ifdef ErrorTrackerExample
+#	include "ErrorTracker.ex.cpp"
+#endif
+
+#ifdef LoggerNDebugExample
+#	include "logger.ndbg.ex.cpp"
+#endif
+
+#ifdef LoggerDebugExample
+#	include "logger.dbg.ex.cpp"
+#endif
+
+#ifdef QueuedHandlerExample
+#	include "QueuedHandler.ex.cpp"
+#endif
+
+#ifdef TimerExample
+#	include "Timer.ex.cpp"
+#endif
+
+#ifdef TimeStampExample
+#	include "TimeStamp.ex.cpp"
+#endif
+
+#ifdef TristateExample
+#	include "Tristate.ex.cpp"
+#endif
+
+#ifdef VersionInfoExample
+#	include "VersionInfo.ex.cpp"
+#endif
